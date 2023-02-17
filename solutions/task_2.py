2f79cdff038a1452d9b2dad732acf157340da88f
Sam Komarov <sam@skomarov.com>
2023-02-17 15:47:38 +0300

    write tests for task #2

--- solutions/task_2.py
+++ solutions/task_2.py
@@ -1,8 +1,11 @@
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
+def solve(x1, y1, x2, y2):
+    same_quarter = (x1>0) == (x2>0) and (y1>0) == (y2>0)
 
-same_quarter = (x1>0) == (x2>0) and (y1>0) == (y2>0)
+    return "YES" if same_quarter else "NO"
 
-print("YES" if same_quarter else "NO")
+if __name__ == '__main__':
+    x1 = int(input())
+    y1 = int(input())
+    x2 = int(input())
+    y2 = int(input())
+    print(solve(x1, y1, x2, y2))

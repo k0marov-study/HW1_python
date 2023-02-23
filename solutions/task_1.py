24c990749ccb42c6f2951511039f135e23ce99c4
Sam Komarov <sam@skomarov.com>
2023-02-23 17:48:45 +0300

    refactor task #1

--- solutions/task_1.py
+++ solutions/task_1.py
@@ -2,12 +2,9 @@ k = int(input())
 m = int(input())
 n = int(input())
 
-full_time = 0
-iteration_count = n//k
-iteration_time = 2*m
-full_time += iteration_count * iteration_time
+full_time = n//k * 2*m
 if n < k:
-    full_time += iteration_time
+    full_time += 2*m
 elif n % k != 0:
-    full_time += m if 2*(n % k) <= k else 2 * m
+    full_time += m if 2*(n % k) <= k else 2*m
 print(full_time)

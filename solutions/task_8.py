7881204d0146e35dc6b68593abe91747f0a81dcf
Sam Komarov <sam@skomarov.com>
2023-02-24 10:33:00 +0300

    refactor task #8

--- solutions/task_8.py
+++ solutions/task_8.py
@@ -1,29 +1,31 @@
+PLACE_RIGHT = "RIGHT"
+PLACE_LEFT = "LEFT"
 n = int(input())
 k = int(input())
 petya_row = int(input())  # starting from 1
-petya_place = int(input())
-petya_place -= 1
+petya_place = PLACE_RIGHT if int(input()) == 1 else PLACE_LEFT
 
-petya_position = 2*(petya_row-1) + 1 + petya_place
+petya_position = 2*(petya_row-1) + 1 + (0 if petya_place == PLACE_RIGHT else 1)
 
 row_count = (n+1) // 2
 
-best_row = 0
-best_place = ""
+best_row = None
+best_place = None
 
 for pos in (petya_position - k, petya_position + k):
     row = (pos+1) // 2
-    place = 1 - pos % 2
+    place = PLACE_RIGHT if pos % 2 else PLACE_LEFT
 
-    alone_and_left = row == row_count and n % 2 and place == 1
+    alone_and_left = row == row_count and n % 2 and place == PLACE_LEFT
     pos_valid = 1 <= row <= row_count and not alone_and_left
+
     diff = abs(petya_row - row)
-    best_diff = abs(petya_row - best_row)
-    if pos_valid and (not best_row or diff <= best_diff):
+    pos_better = best_row is None or diff <= abs(petya_row - best_row)
+    if pos_valid and pos_better:
         best_row = row
         best_place = place
 
-if best_row:
-    print(best_row, best_place+1)
-else:
+if best_row is None:
     print(-1)
+else:
+    print(best_row, 1 if best_place == PLACE_RIGHT else 2)

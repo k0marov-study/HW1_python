34be0a5eb4eb1d9e815a356a79311e9df026ae0d
Sam Komarov <sam@skomarov.com>
2023-02-24 12:49:36 +0300

    add tests for task #1

--- solutions/task_1.py
+++ solutions/task_1.py
@@ -1,10 +1,14 @@
-k = int(input())
-m = int(input())
-n = int(input())
+def solve(k, m, n):
+    full_time = n//k * 2*m
+    if n < k:
+        full_time += 2*m
+    elif n % k != 0:
+        full_time += m if 2*(n % k) <= k else 2*m
+    return full_time
 
-full_time = n//k * 2*m
-if n < k:
-    full_time += 2*m
-elif n % k != 0:
-    full_time += m if 2*(n % k) <= k else 2*m
-print(full_time)
+
+if __name__ == '__main__':
+    k = int(input())
+    m = int(input())
+    n = int(input())
+    print(solve(k, m, n))

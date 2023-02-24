96eb54257bacc80d2c2a32505ba1c599b4fcdb6e
Sam Komarov <sam@skomarov.com>
2023-02-24 12:57:44 +0300

    add tests for task #3

--- solutions/task_3.py
+++ solutions/task_3.py
@@ -1,26 +1,26 @@
 SMALL = 1
 MIDDLE = 10
 BIG = 60
-
 BIG_CHEAPER_MIDDLE = 35
 MIDDLE_CHEAPER_SMALL = 9
 
-n = int(input())
 
-big_amount = n // BIG
-n %= BIG
+def solve(n):
+    big_amount = n // BIG
+    n %= BIG
+    if n >= BIG_CHEAPER_MIDDLE:
+        return 0, 0, big_amount+1
 
-if n >= BIG_CHEAPER_MIDDLE:
-    n = 0
-    big_amount += 1
+    middle_amount = n // MIDDLE
+    n %= MIDDLE
+    if n >= MIDDLE_CHEAPER_SMALL:
+        return 0, middle_amount+1, big_amount
 
-middle_amount = n // MIDDLE
-n %= MIDDLE
+    small_amount = n
+    return small_amount, middle_amount, big_amount
 
-if n >= MIDDLE_CHEAPER_SMALL:
-    n = 0
-    middle_amount += 1
 
-small_amount = n
-
-print(small_amount, middle_amount, big_amount)
+if __name__ == '__main__':
+    n = int(input())
+    small, middle, big = solve(n)
+    print(small, middle, big)

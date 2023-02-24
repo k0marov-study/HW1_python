008aead486e4eea3ed92d48661e9cb83e28be641
Sam Komarov <sam@skomarov.com>
2023-02-24 13:02:02 +0300

    add tests for task #7

--- solutions/task_7.py
+++ solutions/task_7.py
@@ -1,10 +1,14 @@
-n = int(input())
-word = ""
-if 10 <= n <= 20 or n % 10 == 0 or 5 <= n % 10 <= 9:
-    word = "korov"
-elif n % 10 == 1:
-    word = "korova"
-else:
-    word = "korovy"
+def solve(n):
+    word = ""
+    if 10 <= n <= 20 or n % 10 == 0 or 5 <= n % 10 <= 9:
+        word = "korov"
+    elif n % 10 == 1:
+        word = "korova"
+    else:
+        word = "korovy"
+    return f"{n} {word}"
 
-print(n, word)
+
+if __name__ == '__main__':
+    n = int(input())
+    print(solve(n))
